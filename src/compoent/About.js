29938d661a61e6fce4ba68d67039114ccd879234
hparvez905 <hparvez905@gmail.com>
2023-03-26 16:15:06 +0600

    navbar item  added

--- src/compoent/About.js
+++ src/compoent/About.js
@@ -1,10 +1,19 @@
 import React from "react"
+import bg from '../assets/about.jpg'
 
 const About = () => {
   return (
     <>
-      <section className='hero'>
-        <h1>Welcome To About Page</h1>
+     <section>
+        
+        <div >
+        <img src={bg} alt=""className="w-full h-96"/>
+        </div>
+        <div>
+          <p className="m-auto p-4 mx-40 my-6">
+To find out how World BioHazTec can assist you and your team, please contact us at a location listed below, call +65 6634 2600 or email at <span className="hover:underline text-sky-600">Lng@worldbiohaztec.com</span>. We typically respond within two business days.</p>
+        </div>
+        
       </section>
     </>
   )
