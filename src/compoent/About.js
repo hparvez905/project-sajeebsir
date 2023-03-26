23ae263ec903558293d5b87159c9470e264e58c9
hparvez905 <hparvez905@gmail.com>
2023-03-26 17:30:54 +0600

    about us  added

--- src/compoent/About.js
+++ src/compoent/About.js
@@ -1,5 +1,6 @@
 import React from "react"
 import bg from '../assets/about.jpg'
+import { AboutDetails } from "./about/AboutDetails"
 
 const About = () => {
   return (
@@ -10,8 +11,7 @@ const About = () => {
         <img src={bg} alt=""className="w-full h-96"/>
         </div>
         <div>
-          <p className="m-auto p-4 mx-40 my-6">
-To find out how World BioHazTec can assist you and your team, please contact us at a location listed below, call +65 6634 2600 or email at <span className="hover:underline text-sky-600">Lng@worldbiohaztec.com</span>. We typically respond within two business days.</p>
+         <AboutDetails/>
         </div>
         
       </section>
---
+++ src/compoent/about/AboutDetails.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const AboutDetails = () => {
+  return (
+    <div className='p-24 mx-32'>
+        <div>
+            <h1 className='text-4xl mb-4  '>WHAT WE DO: BIOCONTAINMENT CONSULTING AND CERTIFICATION SERVICES</h1>
+            <p className='mb-6 text-base text-gray-600'>World BioHazTec is on an unrelenting mission to make a difference in having research performed safely. As one of the first private biocontainment certification firms, we are your partner in achieving groundbreaking research while practicing safe science. We measure our success by your success.</p>
+            <p className='text-base text-gray-600'>We believe that individuals in research deserve to work in a safe and healthy environment. Our risk assessments are built on years of experience with biocontainment laboratories. This allows us to give you the practical tools and knowledge that you need to have your laboratory operate safely. Whether you are building a new laboratory, renovating, preparing for a CDC Select Agent inspection, or conducting an annual laboratory certification, we would like to become a part of your team throughout the project to ensure that a successful outcome is achieved</p>
+
+        </div>
+        <div className='my-12'>
+            <h1  className='text-4xl mb-4'>WHO WE SERVE: HEALTH AND SAFETY LEADERS</h1>
+            <p className='mb-6 text-base text-gray-600'>We work with government agencies, hospitals, universities, research foundations, pharmaceutical companies, architectural and engineering firms, commissioning agents, and global security companies throughout the world. We are a strategic partner that works with laboratories that are conducting research on a variety of agents including Tuberculosis, SARS, Avian influenza, smallpox, and Bacillus anthracis, to name a few. We have provided design solutions, saved research dollars, made laboratories energy efficient, trained thousands, resolved HVAC issues, provided guidance on government regulations and guidelines, and set up secure and sustaining laboratories.</p>
+            <p className='text-base text-gray-600'>What’s the common thread? Our clients are research leaders in the biosafety field, where great user experience and world class safety management system execution ensures that research is continued, uninterrupted</p>
+            
+        </div>
+        <div>
+            <h1  className='text-4xl mb-4'>HOW WE ARE DIFFERENT: INNOVATION THROUGH EXECUTION</h1>
+            <p className='text-base text-gray-600'>We believe that laboratory certification is achieved by using the combined talents of a team of biocontainment engineers and biosafety professionals who utilize cutting edge technology. We deliver innovation as a designed outcome, not happenstance. No other certification company can match the number of National Institute of Standards and Technology (NIST) certified TSI meters WBHT owns and maintains. We have enough TSI meters to certify a full Vivarium in one day. That means you can get back into your laboratory in minimal time and continue your research. We have worked with laboratories that use the most advanced technologies for conducting research including bioimaging, telemetry, effluent decontamination systems, and stem cell research. World BioHazTec has inspected hundreds of laboratories throughout the United States and the world, which has given us a global perspective on how laboratories operate. We know what it takes to have a laboratory function safely and securely at your location.</p>
+        </div>
+    </div>
+  )
+}
