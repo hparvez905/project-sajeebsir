37952d0220d33c311480fea422f583cd12a1e9e3
hparvez905 <hparvez905@gmail.com>
2023-03-27 12:58:02 +0600

    update home page

--- src/compoent/Home.js
+++ src/compoent/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { HomePart } from './HomePart';
 import Slider from './slider/Slider';
 
 export const Home = () => {
   return (
-    <Slider/>
+    <div>
+      <div className='relative'>
+      <Slider/>
+      </div>
+    <div>
+    <HomePart/>
+    </div>
+    </div>
   )
 }
 export default Home;
