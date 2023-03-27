1bc79a9a2b96a03c0a6572eb2f261e97494292a8
hparvez905 <hparvez905@gmail.com>
2023-03-27 13:01:08 +0600

    update home page

--- src/compoent/HomePart.js
+++ src/compoent/HomePart.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const HomePart = () => {
   return (
     
-        <div className="bg-red-600 h-96 w-1/3  ml-60 absolute top-[50%]  ">
+        <div className="bg-red-600 h-96 w-1/3  ml-48 absolute top-[50%]  ">
           <div className="p-8 ml-6">
           <p className="text-xl font-semibold text-gray-400 mb-4">WELCOME TO ABCD</p>
           <p className="text-6xl mt-10 mb-4 text-white">Solutions <br></br> for Critical<br></br> Environments 
