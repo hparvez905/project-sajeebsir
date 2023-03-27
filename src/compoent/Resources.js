f43fcb7d9b48c2f47201865adbefe7a556f4722f
hparvez905 <hparvez905@gmail.com>
2023-03-27 12:25:16 +0600

    update RESOURCES page

--- src/compoent/Resources.js
+++ src/compoent/Resources.js
@@ -6,8 +6,14 @@ const Resources = () => {
     <>
       <section>
         
-        <div >
+        <div className="relative">
         <img src={bg} alt=""className="w-full h-96"/>
+        <div className="bg-white h-36 w-1/3  ml-60 absolute top-[25%]  ">
+          <div className="p-2 mt-2 ml-6">
+          <p className="text-md font-semibold text-red-600 mb-2">RESOURCES</p>
+          <p className="text-3xl mb-4">Additional info for your references</p>
+          </div>
+        </div>
         </div>
         <div className="mx-48 p-6">
           <h1 className="text-4xl">United States Guidelines and Regulations</h1>
